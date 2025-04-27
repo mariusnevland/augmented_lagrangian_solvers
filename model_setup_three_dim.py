5f500dd05528a4b09fe7d0dc35efa776790c8476
mariusnevland <mne029@uib.no>
2025-04-27 11:36:39 +0200

    Initial draft of 3D simulation

--- model_setup_three_dim.py
+++ model_setup_three_dim.py
@@ -1,6 +1,6 @@
 import numpy as np
 import porepy as pp
-from typing import Literal
+from typing import Literal, Callable
 
 
 class EllipticFractureNetwork:
@@ -127,4 +127,140 @@ class HydrostaticPressureGradient3D:
         all_bf, *_ = self.domain_boundary_sides(boundary_grid)
         values[all_bf] = self.hydrostatic_pressure(depth)
         return values
+    
+
+class ConstrainedPressureEquaton3D:
+ 
+     def mass_balance_equation(self, subdomains: list[pp.Grid]) -> pp.ad.Operator:
+         eq = self.pressure(subdomains) - pp.ad.Scalar(self.units.convert_units(2e7, "Pa"))
+         eq.set_name("new mass balance")
+         return eq
+     
+
+class PressureConstraintWell3D:
+
+    def update_time_dependent_ad_arrays(self) -> None:
+        """Set current injection pressure."""
+        super().update_time_dependent_ad_arrays()
+ 
+        # Update injection pressure
+        current_injection_pressure = self.units.convert_units(2e7, "Pa") + self.units.convert_units(1e7, "Pa")
+        for sd in self.mdg.subdomains(return_data=False):
+            pp.set_solution_values(
+                name="current_injection_pressure",
+                values=np.array([current_injection_pressure]),
+                data=self.mdg.subdomain_data(sd),
+                iterate_index=0,
+            )
+ 
+    def _fracture_center_cell(self, sd: pp.Grid) -> np.ndarray:
+        # Compute the fracture cell that is closest to the center of the fracture
+        mean_coo = np.mean(sd.cell_centers, axis=1).reshape((3, 1))
+        center_cell = sd.closest_cell(mean_coo)
+        return center_cell
+ 
+    def mass_balance_equation(self, subdomains: list[pp.Grid]) -> pp.ad.Operator:
+        std_eq = super().mass_balance_equation(subdomains)
+ 
+        # Need to embedd in full domain
+        sd_indicator = [np.zeros(sd.num_cells) for sd in subdomains]
+ 
+        # Pick the only subdomain
+        fracture_sds = [sd for sd in subdomains if sd.dim == self.nd - 1]
+ 
+        if len(fracture_sds) == 0:
+            return std_eq
+ 
+        # Pick a single fracture
+        well_sd = fracture_sds[0]
+ 
+        for i, sd in enumerate(subdomains):
+            if sd == well_sd:
+ 
+                well_loc_ind = self._fracture_center_cell(sd)
+                sd_indicator[i][well_loc_ind] = 1
+ 
+        # Characteristic functions
+        indicator = np.concatenate(sd_indicator)
+        reverse_indicator = 1 - indicator
+ 
+        current_injection_pressure = pp.ad.TimeDependentDenseArray(
+            "current_injection_pressure", [self.mdg.subdomains()[0]]
+        )
+        constrained_eq = self.pressure(subdomains) - current_injection_pressure
+ 
+        eq_with_pressure_constraint = (
+            pp.ad.DenseArray(reverse_indicator) * std_eq
+            + pp.ad.DenseArray(indicator) * constrained_eq
+        )
+        eq_with_pressure_constraint.set_name(
+            "mass_balance_equation_with_constrained_pressure"
+        )
+ 
+        return eq_with_pressure_constraint
+    
+
+class HydrostaticPressure:
+    """Utility class to compute (generalized) hydrostatic pressure."""
+
+    fluid: pp.FluidComponent
+
+    def hydrostatic_pressure(self, sd: pp.Grid) -> np.ndarray:
+        p_atm = 0
+        gravity = self.units.convert_units(pp.GRAVITY_ACCELERATION, "m*s^-2")
+        rho = self.params["material_constants"]["fluid"].constants_in_SI["density"]
+        rho_scaled = self.units.convert_units(rho, "Pa")
+        rho_g = rho_scaled * gravity
+        # We assume the top of the domain is at 1km depth.
+        z = self.units.convert_units(3000, "m") - sd.cell_centers[self.nd - 1] 
+        pressure = p_atm + rho_g * z
+        return pressure
+
+    def update_time_dependent_ad_arrays(self) -> None:
+        """Set hydrostatic pressure for current gravity."""
+        super().update_time_dependent_ad_arrays()
+
+        # Update injection pressure
+        for sd in self.mdg.subdomains(return_data=False):
+            hydrostatic_pressure = self.hydrostatic_pressure(sd)
+            pp.set_solution_values(
+                name="hydrostatic_pressure",
+                values=np.array(hydrostatic_pressure),
+                data=self.mdg.subdomain_data(sd),
+                iterate_index=0,
+            )
+
+
+class HydrostaticPressureBC(HydrostaticPressure):
+
+    # Pressure gradient directly based on the hydrostatic pressure, i.e 
+    # the pressure will linearly increase from top to bottom, according to
+    # p=rho*g*z.
+
+    # Dirichlet (pressure) conditions on all boundaries.
+
+    domain_boundary_sides: Callable[[pp.Grid | pp.BoundaryGrid], pp.domain.DomainSides]
+
+    def _fluid_pressure_boundary_faces(self, sd: pp.Grid) -> np.ndarray:
+        """Auxiliary method to identify all Dirichlet/pressure boundaries."""
+        domain_sides = self.domain_boundary_sides(sd)
+        return domain_sides.all_bf
+
+    def bc_type_darcy_flux(self, sd: pp.Grid) -> pp.BoundaryCondition:
+        return pp.BoundaryCondition(sd, self._fluid_pressure_boundary_faces(sd), "dir")
+
+    def bc_type_fluid_flux(self, sd: pp.Grid) -> pp.BoundaryCondition:
+        return pp.BoundaryCondition(sd, self._fluid_pressure_boundary_faces(sd), "dir")
+
+    def bc_values_pressure(self, boundary_grid: pp.BoundaryGrid) -> np.ndarray:
+        pressure = np.zeros(boundary_grid.num_cells)
+
+        # Apply hydrostatic pressure on all sides of the domain.
+        if boundary_grid.dim == self.nd - 1:
+            sides = self.domain_boundary_sides(boundary_grid)
+            pressure[sides.all_bf] = self.hydrostatic_pressure(boundary_grid)[
+                sides.all_bf
+            ]
+
+        return pressure
     
--- run_three_dim.py
+++ run_three_dim.py
@@ -12,17 +12,82 @@ from export_iterations import *
 from contact_mechanics_mixins import *
 
 
-class ThreeDimInit(EllipticFractureNetwork,
-                   LithoStaticTraction3D,
-                   HydrostaticPressureGradient3D,
-                   pp.constitutive_laws.GravityForce,
-                   NormalPermeabilityFromSecondary,
-                   NewtonReturnMapTest,
-                   pp.constitutive_laws.CubicLawPermeability,
-                   pp.poromechanics.Poromechanics):
+class InitializePressure(EllipticFractureNetwork,
+                         HydrostaticPressureGradient3D,
+                         LebesgueConvergenceMetrics,
+                         pp.constitutive_laws.GravityForce,
+                         NormalPermeabilityFromSecondary,
+                         pp.constitutive_laws.CubicLawPermeability,
+                         pp.fluid_mass_balance.SinglePhaseFlow):
     pass
 
-
-params = params_testing_3D
-model = ThreeDimInit(params)
+params = copy.deepcopy(params_initialize_pressure_3D)
+model = InitializePressure(params)
 pp.run_time_dependent_model(model, params)
+vals = model.equation_system.get_variable_values(iterate_index=0)
+
+
+
+
+
+class InitializeMechanics(EllipticFractureNetwork,
+                          LithoStaticTraction3D,
+                          HydrostaticPressureGradient3D,
+                          LebesgueConvergenceMetrics,
+                          AlternativeTangentialEquation,
+                          ContactMechanicsConstant,
+                          DimensionalContactTraction,
+                          pp.constitutive_laws.GravityForce,
+                          NormalPermeabilityFromSecondary,
+                          pp.constitutive_laws.CubicLawPermeability,
+                          pp.poromechanics.Poromechanics):
+    pass
+
+# class ThreeDimInjectionInit(EllipticFractureNetwork,
+#                             LithoStaticTraction3D,
+#                             HydrostaticPressureGradient3D,
+#                             LebesgueConvergenceMetrics,
+#                             AlternativeTangentialEquation,
+#                             ContactMechanicsConstant,
+#                             DimensionalContactTraction,
+#                             pp.constitutive_laws.GravityForce,
+#                             NormalPermeabilityFromSecondary,
+#                             pp.constitutive_laws.CubicLawPermeability,
+#                             pp.poromechanics.Poromechanics):
+#     pass
+
+
+# def get_initial_condition():
+#     params = copy.deepcopy(params_testing_3D)
+#     model = ThreeDimInjectionInit(params)
+#     pp.run_time_dependent_model(model, params)
+#     vals = model.equation_system.get_variable_values(iterate_index=0)
+#     return vals
+
+# init_cond = get_initial_condition()
+# print(np.linalg.norm(init_cond))
+
+# class InitialCondition:
+#     def initial_condition(self) -> None:
+#         super().initial_condition()
+#         self.equation_system.set_variable_values(
+#             init_cond,
+#             time_step_index=0,
+#             iterate_index=0,
+#         )
+
+
+# class ThreeDimInjection(InitialCondition,
+#                         EllipticFractureNetwork,
+#                         # PressureConstraintWell
+#                         LithoStaticTraction3D,
+#                         HydrostaticPressureGradient3D,
+#                         LebesgueConvergenceMetrics,
+#                         AlternativeTangentialEquation,
+#                         DimensionalContactTraction,
+#                         NormalPermeabilityFromSecondary,
+#                         pp.constitutive_laws.GravityForce,
+#                         pp.constitutive_laws.CubicLawPermeability,
+#                         pp.poromechanics.Poromechanics):
+#     pass
+
