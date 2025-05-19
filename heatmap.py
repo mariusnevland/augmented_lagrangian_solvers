c27950ce5c1cdb35f0c097fc5225126db6ab805a
mariusnevland <mne029@uib.no>
2025-05-19 10:49:59 +0200

    Update heatmap.py

--- heatmap.py
+++ heatmap.py
@@ -28,7 +28,9 @@ def heatmap(data: np.ndarray,
     # TODO: Document inputs to function.
     df = pd.DataFrame(data)
     df = df.astype(int)
-    annot = df.astype(str).replace("0", "NC").replace("-1", "Div")
+    annot = df.astype(str).replace("0", "NC").replace("500", "Div")
+    if annot[3][3] == "100":
+        annot[3][3] = "220"
     plt.figure(figsize=(10,6))
     # cmap = sns.light_palette("#C5001A", as_cmap=True)
     # Load base colormap
@@ -38,7 +40,7 @@ def heatmap(data: np.ndarray,
     cmap = trunc_cmap
     # cmap = sns.color_palette("YlOrRd", as_cmap=True)
     cmap.set_under('#908D8D')
-    # cmap.set_over('#696969')
+    cmap.set_over('#696969')
     # cmap.set_over('green')
     mesh = sns.heatmap(df, linewidths=0.5, xticklabels=xticks,
                    yticklabels=yticks,
@@ -79,13 +81,37 @@ ex1_medium_new_ref = np.array([[ 44,  44,  43,  44,   0,   0,   0],
  [ 44,  44,  43,  44,  55,  61,  72],
  [ -1,  -1,   0, 820,   0,   0,   0]])
 
+ex1_easy_new_ref_one_time = np.array([[5,  5,  5,  5,  5,  7,  0],
+ [ 5,  5,  5,  5,  5,  6,  5],
+ [ 5,  5,  5,  5,  5,  7, 10],
+[14854, 2182,  292,   53,   20,  13,    0]])
+
+ex1_medium_new_ref_one_time = np.array([[16, 16, 16, 16,  0,  0,  0],
+ [16, 16, 16, 18, 17, 21, 16],
+ [16, 16, 16, 16, 24, 27, 27],
+[ 500,  500,   0, 100,   0,   0,   0]])  # 4th column is really 220
+
+ex1_hard_new_ref_one_time = np.array([[ 0,  0, 500,  0,  0,  0,  0],
+ [500, 29, 26, 16, 21, 55, 35],
+ [85, 69, 500, 500, 54, 78, 52],
+ [500,  0, 500, 500,  0,  0,  0]])
+
 
 ex1_hard_new_ref = np.array([[  0,   0,  -1,   0,   0,   0,   0],
  [ -1,  65,  58,  49,  57,  91,  69],
  [ 82,  87,  -1,  -1,  94, 142, 126],
  [ -1,   0,  -1,  -1,   0,   0,   0]])
 
+ex1_grid = np.array([[47, 49, 51, 54],
+                    [51, 51, 60, 68],
+                    [0, 0, 76, 68],
+                    [58, 84, 84, 86]])
+
 c_vals = ["1e-3", "1e-2", "1e-1", "1e0", "1e1", "1e2", "1e3"]
 solvers = ["Newton", "NRM", "ANRM", "CRM"]
-heatmap(data=ex1_hard_new_ref, vmin=10, vmax=200, xticks=c_vals, yticks=solvers,
-        xlabel="c-parameter", file_name="ex1_hard_new_ref")
+xticks_grid = ["4300", "11415", "32122", "63370"]
+yticks_grid = ["Newton, well pressure 21MPa", "NRM, well pressure 21MPa",
+               "Newton, well pressure 25MPa", "NRM, well pressure 25MPa"]
+xlabel_grid = "Number of cells"
+heatmap(data=ex1_grid, vmin=1, vmax=200, xticks=xticks_grid, yticks=yticks_grid,
+        xlabel=xlabel_grid, file_name="ex1_grid")
