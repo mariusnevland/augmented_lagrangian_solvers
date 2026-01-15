a7fc83651149e00ff71a5f3beb533e3f78ce25d5
mariusnevland <mne029@uib.no>
2026-01-15 10:35:00 +0000

    Update runscript_nonlinearities.py

--- runscript_nonlinearities.py
+++ runscript_nonlinearities.py
@@ -4,10 +4,12 @@ os.environ["NUMEXPR_NUM_THREADS"] = "1"
 os.environ["OMP_NUM_THREADS"] = "1"
 import numpy as np
 import porepy as pp
+import pp_solvers
 import copy
 from model_setup_two_dim import *
 from plot_utils import *
 from parameters import *
+from linesearch import *
 from model_setup_common import *
 from postprocessing_mixins import *
 from porepy.applications.test_utils.models import add_mixin
@@ -15,6 +17,7 @@ from porepy.applications.test_utils.models import add_mixin
 # Runscript for producing figure ? in the article.
 
 class SimpleInjectionInit(FractureNetwork2D,
+                          pp_solvers.IterativeSolverMixin,
                           AnisotropicStressBC,
                           ConstantPressureBC,
                           CustomPressureStress,
@@ -29,6 +32,7 @@ class SimpleInjectionInit(FractureNetwork2D,
     pass
 
 class SimpleInjection(FractureNetwork2D,
+                      pp_solvers.IterativeSolverMixin,
                       PressureConstraintWell,
                       AnisotropicStressBC,
                       ConstantPressureBC,
@@ -42,40 +46,46 @@ class SimpleInjection(FractureNetwork2D,
     pass
 
 
-c_values = [1e-4, 1e-3, 1e-2, 1e-1, 1e0, 1e1, 1e2, 1e3, 1e4]
+c_values = [1e-3, 1e-2, 1e-1, 1e0, 1e1, 1e2, 1e3]
 solvers = ["GNM", "GNM-RM", "IRM"]
-itr_list = []
-nonlinearities = ["no_aperture", "no_cubic_law", "full_model"]
+itr_list = [[] for _ in c_values]
+itr_time_step_list = [[] for _ in c_values]
+itr_linear_list = [[] for _ in c_values]
+nonlinearities = ["No aperture", "No cubic law", "Full model"]
 for nonlin in nonlinearities:
-    for solver in solvers:
-        for c in c_values:
-            params_init = copy.deepcopy(params_initialization)
-            if nonlin == "no_aperture":
-                model_class_init = add_mixin(ConstantAperture, SimpleInjectionInit)
-            elif nonlin == "no_cubic_law":
-                model_class_init = add_mixin(ConstantCubicLawPermeability, SimpleInjectionInit)
-            elif nonlin == "full_model":
-                model_class_init = SimpleInjectionInit
-            model_init = model_class_init(params_init)
-            pp.run_time_dependent_model(model_init, params_init)
-            vals = model_init.equation_system.get_variable_values(iterate_index=0)
-            class InitialCondition:
-                def initial_condition(self) -> None:
-                    super().initial_condition()
-                    self.equation_system.set_variable_values(
-                        vals,
-                        time_step_index=0,
-                        iterate_index=0,
-                    )
-            if nonlin == "no_aperture":
-                model_class = add_mixin(InitialCondition, add_mixin(ConstantAperture, SimpleInjection))
-            elif nonlin == "no_cubic_law":
-                model_class = add_mixin(InitialCondition, add_mixin(ConstantCubicLawPermeability, SimpleInjection))
-            elif nonlin == "full_model":
-                model_class = add_mixin(InitialCondition, SimpleInjection)      
+    params_init = copy.deepcopy(params_initialization)
+    if nonlin == "No aperture":
+        model_class_init = add_mixin(ConstantAperture, SimpleInjectionInit)
+    elif nonlin == "No cubic law":
+        model_class_init = add_mixin(ConstantCubicLawPermeability, SimpleInjectionInit)
+    elif nonlin == "Full model":
+        model_class_init = SimpleInjectionInit
+    model_init = model_class_init(params_init)
+    pp.run_time_dependent_model(model_init, params_init)
+    vals = model_init.equation_system.get_variable_values(iterate_index=0)
+    class InitialCondition:
+        def initial_condition(self) -> None:
+            super().initial_condition()
+            self.equation_system.set_variable_values(
+                vals,
+                time_step_index=0,
+                iterate_index=0,
+            )
+    if nonlin == "No aperture":
+        model_class = add_mixin(InitialCondition, add_mixin(ConstantAperture, SimpleInjection))
+    elif nonlin == "No cubic law":
+        model_class = add_mixin(InitialCondition, add_mixin(ConstantCubicLawPermeability, SimpleInjection))
+    elif nonlin == "Full model":
+        model_class = add_mixin(InitialCondition, SimpleInjection) 
+    for (i, c) in enumerate(c_values):
+        for solver in solvers:     
             params = copy.deepcopy(params_injection_2D)
             params["injection_overpressure"] = 0.1 * 1e7
-            itr_solver = run_and_report_single(Model=model_class, params=params, c_value=c, solver=solver)
-            itr_list.append(itr_solver)
-    itr_list = np.array(itr_list).reshape((len(solvers),len(c_values)))
-    print(itr_list)
+            params["irm_update_strategy"] = True
+            [itr_solver, itr_time_step_list_solver, itr_linear_solver] = run_and_report_single(Model=model_class, params=params, c_value=c, solver=solver)
+            itr_list[i].append(itr_solver)
+            itr_time_step_list[i].append(itr_time_step_list_solver)
+            itr_linear_list[i].append(itr_linear_solver)
+    bar_chart(itr_time_step_list, itr_linear_list, ymin=0, ymax=200, 
+              num_c_values=len(c_values), labels = [f"{x:.0e}" for x in c_values], 
+              file_name="bar_test_func", title=f"Nonlinearity: {nonlin}")
